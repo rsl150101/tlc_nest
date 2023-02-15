0246ece2e9b07847aac5cc11fcb155ef1a9ae4ab
rsl150101 <dnwnd501@naver.com>
2023-02-16 03:25:54 +0900

    서비스 작성 및 자동 데이터 형 변환 설정

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import _ from 'lodash';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  private articles = [];
+  private articlePasswords = new Map(); //- 파이썬의 딕셔너리와 유사한 자료구조
+
+  getArticles() {
+    return this.articles;
+  }
+
+  getArticleById(id: number) {
+    return this.articles.find((article) => article.id === id);
+  }
+
+  createArticle(title: string, content: string, password: number) {
+    const articleId = this.articles.length + 1;
+    this.articles.push({ id: articleId, title, content });
+    this.articlePasswords.set(articleId, password);
+    return articleId;
+  }
+
+  updateArticle(id: number, title: string, content: string, password: number) {
+    if (this.articlePasswords.get(id) !== password) {
+      throw new UnauthorizedException('Password is not correct. id: ' + id);
+      //- Nest.js 는 자체적으로 http 상태코드에 대한 처리와 예외에 대한 생각을 최소화할 수 있게 그러한 예외 처리를 제공해준다
+    }
+
+    const article = this.getArticleById(id);
+    if (_.isNil(article)) {
+      throw new NotFoundException('Article not found. id :' + id);
+    }
+
+    article.title = title;
+    article.content = content;
+  }
+
+  deleteArticle(id: number, password: number) {
+    if (this.articlePasswords.get(id) !== password) {
+      throw new UnauthorizedException('Password is not correct. id: ' + id);
+    }
+
+    this.articles = this.articles.filter((article) => article.id !== id);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule); //- nest 웹어플레케이션을 생성
-  app.useGlobalPipes(new ValidationPipe()); //- DTO 유효성 검사
+  app.useGlobalPipes(new ValidationPipe({ transform: true })); //- DTO 유효성 검사, 데이터 형 변환 설정
   await app.listen(3000); //- 3000 포트로 서버를 띄움
 }
 bootstrap();
